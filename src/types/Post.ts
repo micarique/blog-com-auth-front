6d95b51cfb721ee7d102ecb7855b193ed2e58e5f
Micael Machado <micarique2010@gmail.com>
2025-05-11 11:21:40 -0300

    fix: alteração no nome das variáveis do autor de acordo com a resposta do backend

--- src/types/Post.ts
+++ src/types/Post.ts
@@ -2,8 +2,8 @@ export interface Post {
   id: number;
   titulo: string;
   conteudo: string;
+  authorEmail: string;
+  authorId: string;
+  authorName: string;
   createdAt: string;
-  autor: string;
-  autorId: string;
-  autorEmail: string;
 }
